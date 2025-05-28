b5b21ff4761e2e35ebb6ad2282a86d43e22ab770
Mohammad Ghasemi <mohammad.ghasemi@karizmastudio.com>
2025-05-28 17:29:14 +0330

    json utility added

---
+++ KarizmaCore/Utils/JsonExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace KarizmaPlatform.Core.Utils;
+
+public static class JsonExtensions
+{
+    public static JsonDocument ToJsonDocument(this object obj)
+    {
+        var json = JsonSerializer.Serialize(obj);
+        return JsonDocument.Parse(json);
+    }
+
+    public static string ToJsonString(this object obj)
+    {
+        var json = JsonSerializer.Serialize(obj);
+        return JsonDocument.Parse(json).ToString() ?? "";
+    }
+}
