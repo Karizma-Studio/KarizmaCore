a9879c3bb034e7c3ebafad6fb24a2ffcb0bc11f1
Mohammad Ghasemi <mohammad.ghasemi@karizmastudio.com>
2025-10-15 16:41:31 +0330

    add database to i base context

--- KarizmaCore/Database/IBaseContext.cs
+++ KarizmaCore/Database/IBaseContext.cs
@@ -1,7 +1,10 @@
-﻿namespace KarizmaPlatform.Core.Database
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace KarizmaPlatform.Core.Database
 {
-    public interface IBaseContext
+    public interface IBaseContext : IAsyncDisposable
     {
+        DatabaseFacade Database { get; }
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
         int SaveChanges();
     }
